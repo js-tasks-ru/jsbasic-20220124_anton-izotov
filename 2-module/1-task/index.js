7a16c035e21b0f3ac7c8542268ed48e2720c538b
Che4891 <antoon.izotov@yandex.ru>
2022-02-02 09:12:52 +0200

    added first task in second modules

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries) {
+    if (!isNaN(salaries[key]) && salaries[key] !== Infinity && typeof salaries[key] == 'number' && salaries[key] !== -Infinity) {
+      console.log(salaries[key]);
+      sum += salaries[key];
+    }
+  }
+  return sum;
 }
