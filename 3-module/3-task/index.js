d27f04ecc3f98a85609f95296dff8ff1cb8700b6
Che4891 <antoon.izotov@yandex.ru>
2022-02-06 13:46:29 +0200

    therd task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,6 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('-');
+  let newArr = arr.map((item, index) => index == 0 ? item : item[0].toUpperCase() + item.slice(1));
+  let sumArr = newArr.join('');
+  return sumArr;
 }
