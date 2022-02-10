b80d9051e0171698b7843131ad9758bc3cafbe1b
Che4891 <antoon.izotov@yandex.ru>
2022-02-10 08:05:10 +0200

    added first task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,10 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const body = document.querySelector('body');
+  const ul = document.createElement('ul');
+  body.append(ul);
+  
+  for (let friend of friends) {
+    ul.insertAdjacentHTML('beforeEnd', `<li>${friend.firstName} ${friend.lastName}</li>`);
+  }
+  return ul;
 }
