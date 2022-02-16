6885034e723b2779c8c681c5ec5a1bba06ed3ac7
Che4891 <antoon.izotov@yandex.ru>
2022-02-16 08:30:00 +0200

    added slider

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,31 @@
 function initCarousel() {
-  // ваш код...
+  const liner = document.querySelector(".carousel__inner");
+  const nextBtn = document.querySelector(".carousel__arrow_right");
+  const prevBtn = document.querySelector(".carousel__arrow_left");
+  let position = 0;
+  const list = document.querySelectorAll(".carousel__slide");
+  prevBtn.style.display = "none";
+  for (const item of list) {
+    const width = item.offsetWidth;
+    nextBtn.onclick = function () {
+      position += width;
+      liner.style.transform = `translateX(-${position}px)`;
+      if (position > width * 2) {
+        nextBtn.style.display = "none";
+      }
+      if (position > 0) {
+        prevBtn.style.display = "";
+      }
+    };
+    prevBtn.onclick = function () {
+      position -= width;
+      liner.style.transform = `translateX(${-position}px)`;
+      if (position <= width * 2) {
+        nextBtn.style.display = "";
+      }
+      if (position <= 0) {
+        prevBtn.style.display = "none";
+      }
+    };
+  }
 }
